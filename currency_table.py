0a6044cc3661bc5f48cb2fdb15723862e0b08da5
Kevin Wang <kevinwang101@gmail.com>
2017-11-28 15:21:30 -0500

    add currency dict draft

---
+++ currency_table.py
@@ -0,0 +1,18 @@
+from Collections import defaultdict
+
+class CurrencyTable:
+    def __init__(self):
+        abbrevationDict = defaultdict(list())
+        abbrevationDict["usd"] = (["u.s. dollar", "cent"], ["u.s. dollars", "cents"])
+        abbrevationDict["gbp"] = (["pound", "penny"] , ["pounds", "pence"])
+        abbrevationDict["eur"] = (["euro", "cent"], ["euros", "cents"])
+        abbrevationDict["yen"] = (["yen", "sen", "rin"], ["yens", "sens", "rins"])
+        abbrevationDict["aud"] = (["australian dollar", "cent"], ["australian dollars", "cents"])
+        abbrevationDict["cad"] = (["canadian dollar", "cent"], ["canadian dollars", "cents"])
+        abbrevationDict["chf"] = (["swiss franc", "rappen"], "swiss francs")
+        abbrevationDict["sek"] = (["swedish krona", "ore"], ["swedish kronor", "ore"])
+        abbrevationDict["hkd"] = (["hong kong dollar", "cent"], ["hong kong dollars", "cents"])
+        symbolDict = defaultdict[list()]
+        symbolDict["$"] = ["dollar", "cent"]
+
+        # symbolDict["￥"] = ["yen", "sen"]
