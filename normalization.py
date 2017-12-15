f826e6a26cbcf108d0c5365269675874b435b4d8
Kevin Wang <kevinwang101@gmail.com>
2017-12-15 14:41:56 -0500

    finished implementing normalize_currency functionality

--- normalization.py
+++ normalization.py
@@ -7,7 +7,7 @@ class Normalizer:
         self.abbreviationDict["usd"] = (["u.s. dollar", "cent"], ["u.s. dollars", "cents"])
         self.abbreviationDict["gbp"] = (["pound", "penny"], ["pounds", "pence"])
         self.abbreviationDict["eur"] = (["euro", "cent"], ["euros", "cents"])
-        self.abbreviationDict["yen"] = (["yen", "sen"], ["yens", "sens"])
+        self.abbreviationDict["jpy"] = (["yen", "sen"], ["yens", "sens"])
         self.abbreviationDict["aud"] = (["australian dollar", "cent"], ["australian dollars", "cents"])
         self.abbreviationDict["cad"] = (["canadian dollar", "cent"], ["canadian dollars", "cents"])
         self.abbreviationDict["chf"] = (["swiss franc", "rappen"], "swiss francs")
@@ -20,37 +20,45 @@ class Normalizer:
 
     """"normalize a tokenized currency input into english words"""
     def normalize_currency(self, input):
-        res, currency = [], None
+        res, currency_token = [], input[0].lower()
         # append whole number
-        whole_number = self.normalize_number[1]
-        res.append(whole_number)
+        whole_number = self.normalize_number(input[1])
         # check singular/plural and append currency
-        if whole_number == "one":
-            res.extend([whole_number, self.get_currency(input[0], True)])
+        if whole_number == "one" or whole_number == "zero":
+            res.extend([whole_number, self.get_currency(currency_token, True)])
         else:
-            res.extend([whole_number, self.get_currency(input[0], False)])
+            res.extend([whole_number, self.get_currency(currency_token, False)])
+        if input[2] == '.':
+            if len(input[3]) == 2:
+                cent_number = self.normalize_number(input[3])
+                if cent_number == 'one' or cent_number == "zero":
+                    res.extend(["and", cent_number, self.get_currency(currency_token, True, True)])
+                else:
+                    res.extend(["and", cent_number, self.get_currency(currency_token, False, True)])
+            else:
+                res[1:1] = ["point", self.normalize_decimal(input[3])]
+        return " ".join(res)
 
     """get the currency's english word.
     If is_single is true return singular form (ex: dollar). Otherwise return
     plural form (ex: dollars).
-    Input token(currency toekn
     """
     def get_currency(self, currency_token, is_single, is_cent=False):
         # currency_token is abbreviation
         if currency_token in self.abbreviationDict:
             if not is_cent:
                 return self.abbreviationDict.get(currency_token)[0][0] if is_single else\
-                       self.abbreviationDict.get(currency_token)[0][1]
+                       self.abbreviationDict.get(currency_token)[1][0]
             else:
-                return self.abbreviationDict.get(currency_token)[1][0] if is_single else\
+                return self.abbreviationDict.get(currency_token)[0][1] if is_single else\
                        self.abbreviationDict.get(currency_token)[1][1]
         # currency_token is symbol
         elif currency_token in self.symbolDict:
             if not is_cent:
                 return self.symbolDict.get(currency_token)[0][0] if is_single else\
-                       self.symbolDict.get(currency_token)[0][1]
+                       self.symbolDict.get(currency_token)[1][0]
             else:
-                return self.symbolDict.get(currency_token)[1][0] if is_single else\
+                return self.symbolDict.get(currency_token)[0][1] if is_single else\
                        self.symbolDict.get(currency_token)[1][1]
 
     """normalize the numbers before the decimal points into english words using num2words module.
@@ -77,9 +85,13 @@ class Normalizer:
 
 if __name__ == '__main__':
     normalizer = Normalizer()
-    print(normalizer.get_currency("$", True))
-    print(normalizer.get_currency("usd", True, True))
-    print(normalizer.get_currency("usd", False))
-    print(normalizer.get_currency("jpy", False))
-    print(normalizer.normalize_number('1234'))
-    print(normalizer.normalize_decimal('12345'))
+    # print(normalizer.get_currency("$", True))
+    # print(normalizer.get_currency("usd", True, True))
+    # print(normalizer.get_currency("usd", False))
+    # print(normalizer.get_currency("jpy", False))
+    # print(normalizer.normalize_number('1234'))
+    # print(normalizer.normalize_decimal('12345'))
+    print(normalizer.normalize_currency(('$', '1,000,000,000', '', '')))
+    print(normalizer.normalize_currency(('$', '1,000,000,000', '.', '00')))
+    print(normalizer.normalize_currency(('usd', '10', '.', '789')))
+    print(normalizer.normalize_currency(('jpy', '10', '.', '789')))
