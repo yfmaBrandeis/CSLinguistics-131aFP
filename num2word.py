978598145c8b44d04d774337f25b58dbe4316311
Yifu Chen <chenyifu0413@gmail.com>
2017-12-17 12:31:26 -0500

    num2word modifed

--- num2word.py
+++ num2word.py
@@ -1,12 +1,15 @@
 
-"""Convert an integer number n into a list of english words. input number as a
+"""Convert an integer number to a list of english words. input number as a
  string, break the string into groups of 3 digits, convert each part recursively
  """
 
 
 def num2word(num):
     if len(num) == 1:
-        return [ones[int(num)]]
+        if num == '0':
+            return []
+        else:
+            return [ones[int(num)]]
     elif len(num) == 2:
         if int(num[0]) == 0:
             return num2word(num[1:])
@@ -17,19 +20,17 @@ def num2word(num):
     elif len(num) == 3:
         if int(num[0]) == 0:
             return num2word(num[1:])
-        elif num[1:] == '00':
-            return [ones[int(num[0])], 'hundred']
         else:
-            return [ones[int(num[0])], 'hundred', 'and'] + num2word(num[1:3])
+            return [ones[int(num[0])], 'hundred'] + num2word(num[1:3])
     else:
         sc = (len(num)-1) // 3   # decide the index of scales list
         mod = len(num) % 3       # handle if the length cannot be divided by 3
         if mod != 0:
-            return num2word(num[:mod]) + [scales[sc], ''] + num2word(num[mod:])
+            return num2word(num[:mod]) + [scales[sc]] + num2word(num[mod:])
         elif num[:3] == '000':
             return num2word(num[3:])
         else:
-            return num2word(num[:3]) + [scales[sc], ''] + num2word(num[3:])
+            return num2word(num[:3]) + [scales[sc]] + num2word(num[3:])
 
 
 """convert digits of the input number(string type) to list of english words"""
@@ -44,7 +45,7 @@ ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
         'sixteen', 'seventeen', 'eighteen', 'nineteen']
 tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
         'eighty', 'ninety']
-scales = ['', 'thousand', 'million', 'billion', 'trillion']
+scales = ['hundred', 'thousand', 'million', 'billion', 'trillion']
 digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
           'eight', 'nine']
 
@@ -62,5 +63,3 @@ if __name__ == '__main__':
     print(num2word('100100000'))
     print(num2word('100000000000'))
     print(digit2word('530'))
-
-    # TODO(1): replace all '' in the list with 'and', except trailing ones
