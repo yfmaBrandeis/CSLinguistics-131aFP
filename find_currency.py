6d6e94fa4560bda06bd60d91ba76e6f7f2092742
Yifu Chen <chenyifu0413@gmail.com>
2017-12-17 22:28:44 -0500

    find_currency.py modified

--- find_currency.py
+++ find_currency.py
@@ -3,37 +3,38 @@ from normalization import Normalizer
 import json
 
 
-def regexfind():
-    f = open('./bloomberg/data/bloomberg.json', 'r', encoding='utf8')
-    data = json.load(f)
+def regexfind(s):
 
     # compose re from keys in normalizer's currency dictionaries
     symbols = '\$|£|€|¥|fr|fr\.|krusd|gbp|eur|jpy|aud|cad|chf|sek|hkd'
     scales = 'hundred|thousand|million|billion|trillion'
-    re_currency = re.compile(r'((?i)%s)\s?([\d\,\s]*\d)\.?(\d*)\s?((?i)%s)?' % (symbols, scales))
+    re_currency = re.compile(r'((?i)%s)\s?([\d\,\s]*\d)\.?(\d*)\s?((?i)%s)?'
+                             % (symbols, scales))
 
     # divided into four groups: (currency symbol, integer, decimal, scale)
-    result = []
-    for js in data:
-        temp = re.findall(re_currency, str(js["news_body"]))
-        if temp is not None:
-            result.append(temp)
-        else:
-            result.append([('', '', '', '')])
-    return result
+    return re.findall(re_currency, s)
 
 
 if __name__ == '__main__':
 
-    fout = open('./out/result.txt', 'w', encoding='utf8')
+    f_in = open('./bloomberg/data/bloomberg.json', 'r', encoding='utf8')
+    f_out = open('./out/result.txt', 'w', encoding='utf8')
+    bloomberg_data = json.load(f_in)
+    normalizer = Normalizer()
 
-    res = regexfind()
+    result = []
+    for js in bloomberg_data:
+        temp = regexfind(str(js["news_body"]))
+        if temp is not None:
+            result.append(temp)
+        else:
+            result.append([('', '', '', '')])
 
-    for ls in res:
-        fout.write('============================================\n')
-        for found in ls:
-            fout.write(str(found))
-            fout.write('\n')
-        fout.write('\n')
+    for ls in result:
+        f_out.write('============================================\n')
+        for cur_tuple in ls:
+            f_out.write(str(cur_tuple) + '\t\t' + normalizer.normalize_currency(cur_tuple))
+            f_out.write('\n')
+        f_out.write('\n')
 
 
